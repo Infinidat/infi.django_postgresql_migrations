7dd01fd13f0a5a7919b51a270972b83bc2d365a4
Maxim Marsakov <mmarsakov@infinidat.com>
2023-11-21 09:51:22 +0000

    WIP

--- src/tests/sample_project/urls.py
+++ src/tests/sample_project/urls.py
@@ -13,8 +13,16 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
+
+try:
+    # For django < 4
+    from django.conf.urls import url
+except ImportError:
+    # For django >= 4
+    from django.urls import re_path as url
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
